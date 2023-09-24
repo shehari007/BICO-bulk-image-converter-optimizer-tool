03e32dd254bf8c8f9a73112c5aead4f26a25ccfb
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2023-09-24 14:06:57 +0300

    Electron template initialized & file structure updated

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Conversion from './Components/ConversionModule/Conversion';
+
+function App() {
+  return (
+   <Conversion/>
+  );
+}
+
+export default App;
---
+++ src/Components/ConversionModule/Conversion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Conversion = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Conversion
